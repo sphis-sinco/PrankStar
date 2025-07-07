d59018fc1f4f77bfad6ad8afacc5d704a4ebb509
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-07 18:50:02 -0500

    Level text shit

--- source/menus/LevelSelect.hx
+++ source/menus/LevelSelect.hx
@@ -1,16 +1,58 @@
 package menus;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.text.FlxText;
+
+using StringTools;
 
 class LevelSelect extends FlxState
 {
+	var levels:Array<String> = ['water-balloon', 'whoopee-cushion', 'pie-to-the-face'];
+	var level_difficulties:Array<Int> = [1, 2, 3];
+
+	var levelTextGrp:FlxTypedGroup<FlxText> = new FlxTypedGroup<FlxText>();
+
 	override function create()
 	{
+		add(levelTextGrp);
+
+		reloadLevelText();
+
 		super.create();
 	}
 
 	override function update(elapsed:Float)
 	{
+		if (FlxG.keys.justReleased.R)
+		{
+			trace('Levelselect reload level text hotkey');
+			reloadLevelText();
+		}
+
 		super.update(elapsed);
+	}
+
+	function reloadLevelText()
+	{
+		if (levelTextGrp.length > 0)
+		{
+			for (levelText in levelTextGrp)
+			{
+				trace('Trying to remove the previous text for ${levelText.text}');
+				levelTextGrp.remove(levelText);
+				levelText.destroy();
+			}
+		}
+
+		for (i in 0...levels.length)
+		{
+			var levelText:FlxText = new FlxText(10, 10 + (i * 40), 0, levels[i].toUpperCase().replace('-', ' '), 32);
+			levelText.ID = i;
+			trace('Adding new text for ${levelText.text}');
+
+			levelTextGrp.add(levelText);
+		}
 	}
 }
