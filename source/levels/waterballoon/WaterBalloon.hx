41e471f3ad99355715f45c36954f4ecdadb26279
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-07 19:36:40 -0500

    Water balloon basic mechanics

--- source/levels/waterballoon/WaterBalloon.hx
+++ source/levels/waterballoon/WaterBalloon.hx
@@ -1,12 +1,15 @@
 package levels.waterballoon;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 
 class WaterBalloon extends FlxState
 {
 	var player:FlxSprite;
+	var thrown:Bool = false;
 
 	override function create()
 	{
@@ -19,6 +22,14 @@ class WaterBalloon extends FlxState
 
 	override function update(elapsed:Float)
 	{
+		if (!thrown)
+		{
+			player.setPosition(FlxG.mouse.x - (player.width / 2), FlxG.mouse.y - (player.height / 2));
+
+			if (FlxG.mouse.justReleased)
+				thrown = true;
+		}
+
 		super.update(elapsed);
 	}
 }
