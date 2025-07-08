b11b866dd8ad9f7557e86f20718b11ee49b9f465
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-07 20:05:21 -0500

    hand and whoopie

--- source/levels/whoopiecushion/WhoopieCushion.hx
+++ source/levels/whoopiecushion/WhoopieCushion.hx
@@ -1,16 +1,33 @@
 package levels.whoopiecushion;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.util.FlxColor;
 
 class WhoopieCushion extends FlxState
 {
+	var player:FlxSprite;
+	var whoopie:FlxSprite;
+	var placed:Bool;
+
 	override function create()
 	{
+		whoopie = new FlxSprite().makeGraphic(64, 64, FlxColor.PINK);
+		add(whoopie);
+
+		player = new FlxSprite().makeGraphic(80, 64);
+		add(player);
+
 		super.create();
 	}
 
 	override function update(elapsed:Float)
 	{
+		player.setPosition(FlxG.mouse.x - (player.width / 2), FlxG.mouse.y - (player.height / 2));
+		if (!placed)
+			whoopie.setPosition(player.x, player.y + (whoopie.height / 2));
+
 		super.update(elapsed);
 	}
 }
